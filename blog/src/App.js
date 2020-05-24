215e42635866c325e9bcc9b5accea53cfe8ebdb9
mateuspb <mateusperin@gmail.com>
2020-05-23 21:05:39 -0300

    Add files via upload

---
+++ blog/src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import New from './components/New';
+import Register from './components/Register';
+import firebase from './firebase';
+import './global.css';
+
+class App extends Component {
+
+  state = {
+    firebaseInitialized: true
+  };
+
+componentDidMount(){
+  firebase.isInitialized().then(resultado => {
+    this.setState({firebaseInitialized: resultado});
+  })
+}
+
+  render(){
+    return this.state.firebaseInitialized !== false ? (
+      <BrowserRouter>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/dashboard" component={Dashboard}/>
+        <Route exact path="/dashboard/new" component={New}/>
+        <Route exact path="/register" component={Register}/>
+      </Switch>
+    </BrowserRouter>
+    ) : (
+      <h1>Carregando...</h1>
+    );
+  }
+}
+
+export default App;
